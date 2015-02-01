6065b6770ca56e716c568af9a61ed19f30f065f1
Saleh Hamadeh <salehhamadeh@gmail.com>
2015-01-31 22:25:42 -0500

    Add Express.js server.

---
+++ server.js
@@ -0,0 +1,23 @@
+// Import express and the server configuration file
+var express = require('express');
+var config = require('./config');
+
+// Create the express app
+var app = express();
+
+// Other routes (middleware)
+app.use('/thegoodword', function(req, res) {
+	res.write("TO HELL WITH GEORGIA!!!");
+	res.end();
+})
+
+// Use the static file server middleware
+app.use(express.static(config.public_dir));
+
+// Start listening to requests
+var server = app.listen(config.server_port, function() {
+	var host = server.address().address;
+
+	console.log("Server running at http://%s:%s", host,
+		config.server_port);
+});
