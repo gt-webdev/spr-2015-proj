b113b0fc657559a23ee9c85708646eb1405b9adf
clayton <claytonkucera@gmail.com>
2015-02-01 21:31:50 -0500

    tweaked config setting for public dir

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {}
 
 // Server configuration
 config.server_port = 3000;
-config.public_dir = '.';
+config.public_dir = './public';
 
 // Set require's return value to config
 module.exports = config;
