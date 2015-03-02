f589617fec0ec72252a2ed0f16fcc374feb83da2
ItsMrLin <JerryLin1206@Gmail.com>
2015-03-01 20:49:57 -0500

    template example

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 // Import express and the server configuration file
 var express = require('express');
 var config = require('./config');
+var fs = require('fs')
+
 
 // Create the express app
 var app = express();
+app.set('view engine', 'ejs')
 
 // Other routes (middleware)
 app.use('/thegoodword', function(req, res) {
 	res.write("TO HELL WITH GEORGIA!!!");
 	res.end();
+})
+
+app.use('/template', function(req, res){
+    var myData;
+    fs.readFile('data/resume.json', 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        myData = JSON.parse(data);
+        res.render('template', myData);
+        res.end();
+    });
+    
 })
 
 // Use the static file server middleware
